4b45a68492e1a5bfaee4cae5fb0259c669fbf079
Raghav Vashisht <raghav2011rv@gmail.com>
2023-05-23 17:06:46 +0530

    Initial Commit

---
+++ LayoutBug/LayoutBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutBugApp.swift
+//  LayoutBug
+//
+//  Created by Raghav Vashisht on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
